85c2147be64581e81452de2bba5b2dfe3277163a
Ryoji Kitano Neto <ryoji_kitano@virgo.atlantico.net.br>
2021-08-29 08:31:50 -0300

    add DataContext for work entityframework connection

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -13,5 +13,10 @@ namespace ShopApi.DDD.AspNetCore.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Product>().Property(p => p.Price).HasPrecision(18,2).HasColumnType("decimal");
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,13 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using ShopApi.DDD.AspNetCore;
 using ShopApi.DDD.AspNetCore.Data;
 
 namespace Shop
@@ -21,7 +25,29 @@ namespace Shop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("database"));
+
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+            services.AddAuthentication(x => 
+                {   x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }).AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
+            //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("database"));
+            services.AddDbContext<DataContext>(opt => opt.UseSqlServer(
+                Configuration.GetConnectionString("connectionString"))
+                );
             services.AddScoped<DataContext, DataContext>();
         }
 
@@ -37,6 +63,7 @@ namespace Shop
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
